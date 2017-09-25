c102c9c3e049e592c11cbde586a29e28a4f5c44d
Eric Zhao <ericlzhao28@gmail.com>
2017-09-25 03:20:44 -0700

    Dockerized everything and updated reqs
    Removed aux set temporarily and minor __init__ fix
    
    updated .gitignore
    
    Minor convenicen script modifications

---
+++ clean_project.sh
@@ -0,0 +1,6 @@
+find . -name "__pycache__" -exec rm -rf {} \;
+find . -name ".cache" -exec rm -rf {} \;
+find . -name "*.pyc" -delete
+find . -name "*.swo" -exec rm -rf {} \;
+find . -name "*.swp" -exec rm -rf {} \;
+echo "Success: python cache files and vim checkpoints have been deleted"
---
+++ template/access_template.sh
@@ -0,0 +1,2 @@
+# Access the Docker cluster
+bash -c "clear && docker exec -it template_template_1 sh"
