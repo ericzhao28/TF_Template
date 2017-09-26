205b407850f95800cb3f6475d9144287b1623325
Eric Zhao <ericlzhao28@gmail.com>
2017-09-26 05:52:01 -0700

    overhauled sample sequential preprocessing

--- template/datasets/test_set/preprocessing.py
+++ template/datasets/test_set/preprocessing.py
@@ -7,92 +7,99 @@ import csv
 def preprocess_file(file_path):
   '''
   Return preprocessed dataset from raw data file.
-  Args:
-    - file_path (str): path to raw dataset file
   Returns:
-    - dataset (dict): {"X": np.arr, "Y": np.arr}
+    - dataset (list): X (np.arr), Y (np.arr)
   '''
-  X_flat = featurize_csv(file_path)
-  metadata = metadatize_csv(file_path)
-  X, Y = segment(X_flat, metadata)
-
-  return {"X": X, "Y": Y}
+  return label_data(*segment_data(*load_data(file_path)))
 
 
-def segment(X_flat, metadata):
-  raw = {}
-  for i in range(len(metadata)):
-    if metadata[i]['seq_id'] not in raw:
-      raw[metadata[i]['seq_id']] = {"metadatum":metadata[i], "x":[]}
-    raw[metadata[i]['seq_id']]['x'].append(X_flat[i])
-
-  X = []
-  Y = []
-  for seq_id, data in raw:
-    X.append(shaping_utils.fix_vector_length(data['x'], config.SEQ_LEN))
-    Y.append(data['metadatum']['y'])
-
-  return X, Y
-
-
-def metadatize_csv(file_path):
+def load_data(file_path):
   '''
-  Load in a CSV and parse for metadatas.
+  Load in a CSV and parse for data.
   Args:
-    - file_path (str): path to raw dataset file
+    - file_path (str): path to raw dataset file.
   Returns:
+    - X: np.array([[0.3, 0.3...]...], dtype=float32)
     - metadata: [{info:a, label:x}... (n_classes)]
   '''
+
+  def __featurize_row(row, headers_key):
+    '''
+    Featurize a row into a real-valued vector.
+    '''
+    feature_vector = []
+    for i, value in enumerate(row):
+      if headers_key[i] in config.numerical_fields:
+        feature_vector.append(float(value))
+    assert(len(feature_vector) == len(config.numerical_fields))
+    return np.array(feature_vector, dtype=np.float32)
+
+  def __metadatize_row(row, headers_key):
+    '''
+    Return metadatized dict for given row.
+    '''
+    metadatum = {}
+    for j, value in enumerate(row):
+      if headers_key[j] == config.label_field:
+        metadatum['y'] = value
+      elif headers_key[j] == config.seq_field:
+        metadatum['seq_id'] = value
+    return metadatum
+
+  X = []
   metadata = []
   with open(file_path, 'r') as f:
     for i, row in enumerate(csv.reader(f)):
       if i == 0:
         headers_key = csv_utils.build_headers(row)
         continue
-      metadatum = {}
-      for j, value in enumerate(row):
-        if headers_key[j] == config.label_field:
-          metadatum['y'] = shaping_utils.one_hot(value, config.label_classes)
-        elif headers_key[j] == config.seq_field:
-          metadatum['seq_id'] = value
-      metadata.append(metadatum)
-
-  return metadata
+      X.append(__featurize_row(row, headers_key))
+      metadata.append(__metadatize_row(row, headers_key))
+  return np.array(X, dtype=np.float32), metadata
 
 
-def featurize_csv(file_path):
+def segment_data(X, metadata):
   '''
-  Load in a CSV and parse for features.
+  Segment X into sequences based off of metadata.
   Args:
-    - file_path (str): path to raw dataset file
+    - X (np.array): data to be segmented on first dim.
+    - metadata (list of dicts): metadata used for segmentation,
+      namely 'seq_id' in this implementation.
   Returns:
-    - X: np.arr((n_points, n_features), float32)
+    - new_X (np.array): segmented np.array with +1 rank.
+    - new_metadata: metadata cut to align with new_X.
   '''
-  X = []
-  with open(file_path, 'r') as f:
-    for i, row in enumerate(csv.reader(f)):
-      if i == 0:
-        headers_key = csv_utils.build_headers(row)
-        continue
-      X.append(featurize_row(row, headers_key))
+  new_X = []
+  new_metadata = []
+  seq_map = {}
 
-  return np.array(X, dtype=np.float32)
+  for i in range(len(X)):
+    seq_id = metadata[i]['seq_id']
+    if seq_id not in seq_map:
+      seq_map[seq_id] = len(new_X)
+      new_X.append([X[i]])
+      new_metadata.append(metadata[i])
+    else:
+      new_X[seq_map[seq_id]].append(X[i])
+
+  for i in range(len(new_X)):
+    new_X[i] = shaping_utils.fix_vector_length(new_X[i], config.SEQ_LEN)
+
+  return np.array(new_X, dtype=np.float32), new_metadata
 
 
-def featurize_row(row, headers_key):
+def label_data(X, metadata):
   '''
-  Featurize a row into a real-valued vector
+  Label X based off of metadata.
   Args:
-    - row (list of str): row of a csv from csv.reader
-    - headers_key (dict): maps pos index in row to field name
+    - X (np.array): data to be segmented on first dim.
+    - metadata (list of dicts): metadata holding in this case, 'y'.
   Returns:
-    - feature_vector: np.arr((n_features), float32)
+    - X (np.array): the original X from args.
+    - Y (np.array): the new labels for dataset.
   '''
-  feature_vector = []
-  for i, value in enumerate(row):
-    if headers_key[i] in config.numerical_fields:
-      feature_vector.append(float(value))
-  assert(len(feature_vector) == len(config.numerical_fields))
-
-  return np.array(feature_vector, dtype=np.float32)
+  Y = []
+  for i in range(len(X)):
+    Y.append(shaping_utils.one_hot(metadata[i]['y'], config.label_classes))
+  return X, np.array(Y, dtype=np.int32)
 
