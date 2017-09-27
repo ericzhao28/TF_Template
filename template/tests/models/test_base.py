091ac1383f411097af7af27957b8b376da5a856b
Eric Zhao <ericlzhao28@gmail.com>
2017-09-26 23:56:48 -0700

    Added resuming test case to test_base

--- template/tests/models/test_base.py
+++ template/tests/models/test_base.py
@@ -44,6 +44,42 @@ def test_train():
   tf.reset_default_graph()
 
 
+def test_checkpointing():
+  tf.reset_default_graph()
+  X, Y = load()
+
+  with tf.Session() as tf_sess:
+    model = Primary(tf_sess, data_config)
+    shuffled_dataset = model.shuffle_and_partition(X, Y, 20, 0)
+    del(X)
+    del(Y)
+    model.initialize()
+    model.train(shuffled_dataset['train']['X'], shuffled_dataset['train']['Y'])
+
+    predictions = model.predict(shuffled_dataset['test']['X'])
+    correct = np.equal(np.argmax(predictions, 1), np.argmax(
+        shuffled_dataset['test']['Y'][:len(predictions)], 1))
+    acc = np.mean(correct.astype(np.float32))
+    print("Test initial load acc:", acc)
+    assert(acc > 0.4)
+
+  tf.reset_default_graph()
+
+  with tf.Session() as tf_sess:
+    model = Primary(tf_sess, data_config)
+    model.initialize()
+    model.restore()
+
+    predictions = model.predict(shuffled_dataset['test']['X'])
+    correct = np.equal(np.argmax(predictions, 1), np.argmax(
+        shuffled_dataset['test']['Y'][:len(predictions)], 1))
+    new_acc = np.mean(correct.astype(np.float32))
+    print("Test loaded acc:", new_acc)
+    assert(new_acc > 0.4)
+
+  tf.reset_default_graph()
+
+
 def test_load():
   tf.reset_default_graph()
   X, Y = load()
